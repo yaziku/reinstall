58ab61ebf854a9fe69e1cae9aac444b40e413413
yaziku <noreply.xh@gmail.com>
2025-06-29 13:13:10 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,12 +3,12 @@
 # shellcheck disable=SC2086
 
 set -eE
-confhome=https://raw.githubusercontent.com/bin456789/reinstall/main
-confhome_cn=https://cnb.cool/bin456789/reinstall/-/git/raw/main
-# confhome_cn=https://www.ghproxy.cc/https://raw.githubusercontent.com/bin456789/reinstall/main
+confhome=https://raw.githubusercontent.com/yazaku/reinstall/main
+#confhome_cn=https://cnb.cool/yaziku/reinstall/-/git/raw/main
+confhome_cn=https://www.ghproxy.net/https://raw.githubusercontent.com/yaziku/reinstall/main
 
 # 默认密码
-DEFAULT_PASSWORD=123@@@
+DEFAULT_PASSWORD=123...
 
 # 用于判断 reinstall.sh 和 trans.sh 是否兼容
 SCRIPT_VERSION=4BACD833-A585-23BA-6CBB-9AA4E08E0003
